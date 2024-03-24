e702a5ab7568fb967717a79bde7041e36f5e8da5
Danata-Tolkachyova <tolkacevadanata@gmail.com>
2024-03-24 22:36:15 +0300

    Signed-off-by: Danata-Tolkachyova <tolkacevadanata@gmail.com>

--- myProgram.py
+++ myProgram.py
@@ -1,5 +1,6 @@
 name = input("What your name? (Type here) ")
 lastname = input("What your lastname? (Also here) ")
+print("input len =", len(name) + len(lastname))
 print("Hello " + name + " " + lastname + "!")
 
 input()
